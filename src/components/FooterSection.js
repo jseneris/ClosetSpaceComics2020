668f416d43080c9b686ab2940fbb86d67cdcd5f4
John Seneris <john@covetlyapp.com>
2020-05-31 12:04:05 -0400

    removing logo temporarily because of sizing

--- src/components/FooterSection.js
+++ src/components/FooterSection.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import logo from '../logo125.png';
+// import logo from '../logo125.png';
 
 export const FooterSection = () => {
   return (
     <footer>
-      <div className="row">
-        <img src={logo} alt="logo"></img>
-      </div>
+      <div className="row">{/* <img src={logo} alt="logo"></img> */}</div>
       <div className="row">
         <p>
           Copyright &copy; 2020 by Closet Space Comics. All rights reserved.
