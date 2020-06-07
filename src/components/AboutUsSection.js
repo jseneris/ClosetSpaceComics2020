1031157be3fabf686311e9ac5717afbe84413967
John Seneris <john@covetlyapp.com>
2020-06-07 12:05:37 -0400

    Little bit of clean up on about page

--- src/components/AboutUsSection.js
+++ src/components/AboutUsSection.js
@@ -8,51 +8,22 @@ export const AboutUsSection = () => {
         <div className="col span-1-of-3">
           <h4>Catalog</h4>
           <ul>
-            <li>Search By Title Option</li>
-            <ul>
-              <li>Add</li>
-            </ul>
-            <li>Clean Up Zoom</li>
-            <ul>
-              <li>Fix margins for sections</li>
-              <li>Extend filter to bottom scroll</li>
-              <li>Switch to title issue scroll?</li>
-            </ul>
+            <li>Add search by Title</li>
+            <li>Switch to title issue list in Zoom</li>
           </ul>
         </div>
         <div className="col span-1-of-3">
           <h4>Collection</h4>
           <ul>
-            <li>Location List</li>
-            <ul>
-              <li>Formatting</li>
-            </ul>
-            <li>Box List</li>
-            <ul>
-              <li>Formatting</li>
-            </ul>
-            <li>Display</li>
-            <ul>
-              <li>Formatting</li>
-              <li>Move Books</li>
-              <ul>
-                <li>Change Order Within Box</li>
-                <li>Move Book between Boxes</li>
-              </ul>
-            </ul>
-            <li>Refactor</li>
+            <li>Drag and drop ordering within boxes</li>
+            <li>Move Book between Boxes</li>
           </ul>
         </div>
         <div className="col span-1-of-3">
           <h4>Purchases</h4>
           <ul>
-            <li>Purchase List</li>
-            <ul>
-              <li>Formatting for purchase list</li>
-              <li>Handle end of list for more button</li>
-              <li>Collapse purchase list when purchase selected</li>
-              <li>Refactor</li>
-            </ul>
+            <li>Formatting for purchase list</li>
+            <li>Add Search by date</li>
           </ul>
         </div>
       </div>
